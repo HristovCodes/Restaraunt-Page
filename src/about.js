2cfeaaf5237af7f0ceda8469d34f5e76d706107b
HristovCodes <>
2020-03-21 12:48:32 +0200

    Menu and about section done

--- src/about.js
+++ src/about.js
@@ -1,10 +1,15 @@
 //generate about tab
 const about = (() => {
-  const name = "About";
   const generate = () => {
     let tab = document.createElement("div");
-    tab.innerText = name;
     tab.classList.add("tab");
+    let title = document.createElement("h1");
+    title.innerText = "It’s More Fun In Indigo!";
+    let text = document.createElement("h2");
+    text.innerText =
+      "Lively music, a vibrant crowd, inventive cocktails and the best sushi this side of California. You know when you’re in Indigo because there’s no other place like it. Order up the best sushi in town and delight in our signature rolls or amazing Japanese inspired cuisine. Whatever you choose, it’s all served with a big side of Anything Can Happen. So bring your friends or make new ones. Either way, you’re in for a great time.";
+    tab.appendChild(title);
+    tab.appendChild(text);
     return tab;
   };
   return {
--- src/menu.js
+++ src/menu.js
@@ -3,9 +3,45 @@ const menu = (() => {
   const name = "Menu";
   const generate = () => {
     let tab = document.createElement("div");
-    tab.innerText = name;
+    let title = document.createElement("h1");
+    title.innerText = "Menu";
+    tab.appendChild(title);
+    tab.appendChild(_generateMenu());
     tab.classList.add("tab");
     return tab;
+  };
+
+  const _generateMenu = () => {
+    let items = document.createElement("menu");
+    items.classList.add("menu");
+    let products = [
+      {
+        name: "Albacore with Crispy Onion",
+        price: "$6.95"
+      },
+      {
+        name: "Takoyaki",
+        price: "$5.95"
+      },
+      {
+        name: "Edamame",
+        price: "$3.95"
+      },
+      {
+        name: "Shishito Pepper",
+        price: "$6.95"
+      }
+    ];
+    products.forEach(element => {
+      let text = document.createElement("h2");
+      text.innerText = element.name;
+      let price = document.createElement("p");
+      price.innerText = element.price;
+      items.appendChild(text);
+      items.appendChild(price);
+    });
+
+    return items;
   };
   return {
     generate
